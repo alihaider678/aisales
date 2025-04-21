9326fa9014c92c1b16f5648219c0d64f2bd011b4
Ali Haider <ali.haiyder11@gmail.com>
2025-04-21 19:26:19 +0500

    admin funcionality

--- campaigns/apps.py
+++ campaigns/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 class CampaignsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'campaigns'
+    verbose_name = 'CAMPAIGNS'  # ← Add this line
